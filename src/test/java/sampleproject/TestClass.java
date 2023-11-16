d737d514cde81bfb48952a8936f0e7b93045c4a7
Abhijeet <abhijeet@naarsoft.com>
2023-11-16 18:41:54 +0530

    Initial commit

---
+++ src/test/java/sampleproject/TestClass.java
@@ -0,0 +1,25 @@
+package sampleproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestClass {
+	public static WebDriver driver;
+	
+	@Test
+	public static void launch() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new");
+
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://automateNow.io");
+		driver.quit();
+
+	}
+
+}
